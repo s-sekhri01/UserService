51bc87a8580baf11a1617bb34ef2fe8120f925d1
Saransh Sekhri <saranshsekhri@gmail.com>
2024-03-09 19:17:47 +0530

    Implemented Eureka Client and Service Discovery

--- src/main/java/com/scaler/userservice/Security/SecurityConfig.java
+++ src/main/java/com/scaler/userservice/Security/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     @Order(2)
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll())
                 // Form login handles the redirect to the login page from the
                 // authorization server filter chain
                 .formLogin(Customizer.withDefaults())
